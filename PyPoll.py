daeb89335624afc5588bc9b590f96caac152e1d4
Caleb Kelly <caleb@Calebs-MBP.attlocal.net>
2022-06-15 18:25:23 -0700

    Adding the election audit code.

--- PyPoll.py
+++ PyPoll.py
@@ -1,3 +1,10 @@
+#The data we need to retrieve:
+# 1. The total number of votes cast 
+# 2. A complete list of candidates who recieved votes.
+# 3. The percentage of votes each candidate won.
+# 4. The total number of votes each candidate won.
+# 5. The winner of the election based on popular vote.
+
 #import dependencies
 import csv
 import os
@@ -8,23 +15,86 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 #assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
-#Open the election results and read the file.
+# Initialize a total vote counter.
+total_votes = 0
+
+# Initialize a new list for candidate options
+candidate_options = []
+
+# Initialize a new dict for candidate votes
+candidate_votes = {}
+
+#Winning Cadidate and Winning Count Tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
+
+# Open the election results and read the file.
 with open(file_to_load) as election_data:
-    # To do: read and analyze the data 
+
     # Read the file object with the reader function
     file_reader = csv.reader(election_data)
+
+    #read the header row.
     headers = next(file_reader)
-    print(headers)
 
     # Print each row in the CSV file.
     for row in file_reader:
-        print(row)
+        # Add to the total vote count. 
+        total_votes += 1
+
+         # print the candidate name from each row 
+        candidate_name = row[2]
+
+        # If the candidate does not match any existing candidate
+        if candidate_name not in candidate_options:
+            # Add it to the list of candidates
+            candidate_options.append(candidate_name)
+
+            #tinitialize vote count 
+            candidate_votes[candidate_name] = 0
+
+        #count each vote
+        candidate_votes[candidate_name] += 1
+
+
+#interate through candidate list
+for candidate_name in candidate_votes:
+
+    #get the vote count of each candidate
+    votes = candidate_votes[candidate_name]
+
+    #Calculate the percentage of votes.
+    vote_percentage = float(votes) / float(total_votes) * 100
+
+    # Print the candidate name and percentage of votes
+    # print(f"{candidate_name} : recieved {vote_percentage:.2f}% of the vote.")
+
+    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+
+    if (votes > winning_count) and (vote_percentage > winning_percentage): 
+        winning_count = votes 
+        winning_percentage = vote_percentage
+        winning_candidate = candidate_name 
+
+    winning_candidate_summary = (
+        f"-------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"-------------------------\n")
+print(winning_candidate_summary)
+
+     
+
+
+# print(candidate_options)
+# print the total votes 
+# print(total_votes)
+# print(candidate_votes)
 
 
 
-#The data we need to retrieve:
-# 1. The total number of voites cast 
-# 2. A complete list of candidates who recieved votes.
-# 3. The percentage of votes each candidate won.
-# 4. The total number of votes each candidate won.
-# 5. The winner of the election based on popular vote.
+
+
